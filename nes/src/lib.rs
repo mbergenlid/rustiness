e0861eacdd5256735c91c1171b27b511e62202ec
Marcus Bergenlid <marcus.bergenlid@gmail.com>
2017-01-15 10:34:19 +0100

    Some timing tests.

--- nes/src/lib.rs
+++ nes/src/lib.rs
@@ -21,320 +21,36 @@ impl NES {
     }
 
     pub fn execute(&mut self, memory: &mut Memory) {
-        self.execute_instruction(memory);
-    }
-
-    fn execute_instruction(&mut self, memory: &mut Memory) {
         opcodes::execute_instruction(&mut self.cpu, memory);
     }
+
 }
 
 #[cfg(test)]
-mod tests {
-    use cpu;
+mod test {
+
+    use std::time::Instant;
     use memory::Memory;
-    use opcodes;
 
-    fn test_program(memory: &mut Memory, expected_cpu_states: &[cpu::CPU]) {
+    #[test]
+    fn timing_test() {
+        let mut memory = memory!(
+            0x00A5 => 0xF0,
+            0x00A6 => 0x10,
+            //ADC $05
+            0x8000 => 0x69,
+            0x8001 => 0x05,
+            0x8002 => 0x10
+        );
+
         let mut nes = super::NES::new();
 
-        for &expected_cpu in expected_cpu_states.iter() {
-            nes.execute_instruction(memory);
-            assert_eq!(expected_cpu, nes.cpu);
-        }
-    }
+        let start = Instant::now();
+        nes.execute(&mut memory);
 
-    #[test]
-    fn instruction_test1() {
-        test_program(
-            &mut memory!(
-                0x00A5 => 0xF0,
-                //ADC $05
-                0x8000 => 0x69,
-                0x8001 => 0x05,
+        //One cycle: 500 ns,
+        println!("Took {} ns", start.elapsed().subsec_nanos());
+        panic!("Took {} ms", start.elapsed().subsec_nanos());
 
-                //AND $00
-                0x8002 => 0x29,
-                0x8003 => 0x00,
-                //ORA $05
-                0x8004 => opcodes::ORA_IMMEDIATE,
-                0x8005 => 0x05,
-
-                0x8006 => opcodes::ASL_ACCUMULATOR,
-
-                0x8007 => opcodes::SEC,
-                //SBC $05
-                0x8008 => opcodes::SBC_IMMEDIATE,
-                0x8009 => 0x05,
-
-                0x800A => opcodes::TAX,
-                0x800B => opcodes::TAY,
-                //STX Y
-                0x800C => opcodes::STX_ZERO_PAGE_Y,
-                0x800D => 0x0A,
-                0x800E => opcodes::AND_IMMEDIATE,
-                0x800F => 0x00,
-                0x8010 => opcodes::TAX,
-                //LDX Y
-                0x8011 => opcodes::LDX_ZERO_PAGE_Y,
-                0x8012 => 0x0A
-            ),
-            &[
-                cpu::CpuBuilder::new()
-                    .program_counter(0x8002)
-                    .accumulator(0x05)
-                    .build(),
-                cpu::CpuBuilder::new()
-                    .program_counter(0x8004)
-                    .flags(cpu::ZERO_FLAG)
-                    .accumulator(0x00)
-                    .build(),
-                cpu::CpuBuilder::new()
-                    .program_counter(0x8006)
-                    .accumulator(0x05)
-                    .build(),
-                cpu::CpuBuilder::new()
-                    .program_counter(0x8007)
-                    .accumulator(0x0A) //1010
-                    .build(),
-                cpu::CpuBuilder::new()
-                    .program_counter(0x8008)
-                    .flags(cpu::CARRY_FLAG)
-                    .accumulator(0x0A) //1010
-                    .build(),
-                cpu::CpuBuilder::new()
-                    .program_counter(0x800A)
-                    .flags(cpu::CARRY_FLAG)
-                    .accumulator(0x05)
-                    .build(),
-                cpu::CpuBuilder::new()
-                    .program_counter(0x800B)
-                    .flags(cpu::CARRY_FLAG)
-                    .accumulator(0x05)
-                    .register_x(0x05)
-                    .build(),
-                cpu::CpuBuilder::new()
-                    .program_counter(0x800C)
-                    .flags(cpu::CARRY_FLAG)
-                    .accumulator(0x05)
-                    .register_x(0x05)
-                    .register_y(0x05)
-                    .build(),
-                cpu::CpuBuilder::new()
-                    .program_counter(0x800E)
-                    .flags(cpu::CARRY_FLAG)
-                    .accumulator(0x05)
-                    .register_x(0x05)
-                    .register_y(0x05)
-                    .build(),
-                cpu::CpuBuilder::new()
-                    .program_counter(0x8010)
-                    .flags(cpu::CARRY_FLAG | cpu::ZERO_FLAG)
-                    .register_x(0x05)
-                    .register_y(0x05)
-                    .build(),
-                cpu::CpuBuilder::new()
-                    .program_counter(0x8011)
-                    .flags(cpu::CARRY_FLAG | cpu::ZERO_FLAG)
-                    .register_y(0x05)
-                    .build(),
-                cpu::CpuBuilder::new()
-                    .program_counter(0x8013)
-                    .flags(cpu::CARRY_FLAG)
-                    .register_x(0x05)
-                    .register_y(0x05)
-                    .build()
-            ]
-        )
-    }
-
-    #[test]
-    fn instruction_test2() {
-        test_program(
-            &mut memory!(
-                //ADC $05
-                0x8000 => 0x69,
-                0x8001 => 0x05,
-
-                0x8002 => opcodes::PHA,
-                0x8003 => opcodes::PLP
-            ),
-            &[
-                cpu::CpuBuilder::new()
-                    .program_counter(0x8002)
-                    .accumulator(0x05)
-                    .build(),
-                cpu::CpuBuilder::new()
-                    .program_counter(0x8003)
-                    .stack_pointer(0xFF)
-                    .accumulator(0x05)
-                    .build(),
-                cpu::CpuBuilder::new()
-                    .program_counter(0x8004)
-                    .flags(0x05)
-                    .accumulator(0x05)
-                    .build(),
-            ]
-        );
-    }
-
-    #[test]
-    fn test_subroutine() {
-        test_program(
-            &mut memory!(
-                0x8000 => opcodes::JSR_ABSOLUTE,
-                0x8001 => 0x20,
-                0x8002 => 0x80,
-
-                0x8003 => opcodes::ADC_IMMEDIATE,
-                0x8004 => 0x05,
-
-                //Sub routine
-                0x8020 => opcodes::LDA_IMMEDIATE,
-                0x8021 => 0x01,
-                0x8022 => opcodes::RTS
-            ),
-            &[
-                cpu::CpuBuilder::new()
-                    .program_counter(0x8020)
-                    .stack_pointer(0xFE)
-                    .build(),
-                cpu::CpuBuilder::new()
-                    .program_counter(0x8022)
-                    .stack_pointer(0xFE)
-                    .accumulator(0x01)
-                    .build(),
-                cpu::CpuBuilder::new()
-                    .program_counter(0x8003)
-                    .accumulator(0x01)
-                    .build(),
-                cpu::CpuBuilder::new()
-                    .program_counter(0x8005)
-                    .accumulator(0x06)
-                    .build(),
-            ]
-        )
-    }
-
-    #[test]
-    fn test_add_with_carry_immediate() {
-        test_instruction(
-            &mut memory!(
-                0x8000 => 0x69,
-                0x8001 => 0x05
-            ),
-            cpu::CpuBuilder::new()
-                .program_counter(0x8002)
-                .accumulator(0x05)
-                .build()
-        );
-    }
-
-    #[test]
-    fn test_add_with_carry_zero_page() {
-        test_instruction(
-            &mut memory!(
-                0x8000 => 0x65,
-                0x8001 => 0xAC,
-                0x00AC => 0x0A
-            ),
-            cpu::CpuBuilder::new()
-                .program_counter(0x8002)
-                .accumulator(10)
-                .build()
-        )
-    }
-
-    fn test_instruction(memory: &mut Memory, expected_cpu: cpu::CPU) {
-        let mut nes = super::NES::new();
-        nes.execute_instruction(memory);
-
-        assert_eq!(expected_cpu, nes.cpu);
-    }
-
-    #[test]
-    fn test_branch_equal() {
-        test_branch(cpu::ZERO_FLAG, opcodes::BRANCH_EQUAL, false);
-        test_branch(cpu::ZERO_FLAG, opcodes::BRANCH_NOT_EQUAL, true);
-        test_branch(cpu::NEGATIVE_FLAG, opcodes::BRANCH_MINUS, false);
-        test_branch(cpu::NEGATIVE_FLAG, opcodes::BRANCH_PLUS, true);
-        test_branch(cpu::CARRY_FLAG, opcodes::BRANCH_CARRY_SET, false);
-        test_branch(cpu::CARRY_FLAG, opcodes::BRANCH_CARRY_CLEAR, true);
-        test_branch(cpu::OVERFLOW_FLAG, opcodes::BRANCH_OVERFLOW_SET, false);
-        test_branch(cpu::OVERFLOW_FLAG, opcodes::BRANCH_OVERFLOW_CLEAR, true);
-    }
-
-    fn test_branch(flag: u8, op_code: u8, negative: bool) {
-        {
-            {
-                let mut memory = memory!(
-                    0x8000 => op_code,
-                    0x8001 => 0x06
-                );
-
-                let mut nes = super::NES::new();
-                nes.cpu.set_flags(flag);
-                nes.execute_instruction(&mut memory);
-                let cpu = &nes.cpu;
-                if negative {
-                    assert_eq!(0x8002, cpu.program_counter());
-                } else {
-                    assert_eq!(0x8008, cpu.program_counter());
-                }
-            }
-
-            {
-                let mut memory = memory!(
-                    0x8000 => op_code,
-                    0x8001 => 0b1111_1010 // -6
-                );
-
-                let mut nes = super::NES::new();
-                nes.cpu.set_flags(flag);
-                nes.execute_instruction(&mut memory);
-                let cpu = &nes.cpu;
-                if negative {
-                    assert_eq!(0x8002, cpu.program_counter());
-                } else {
-                    assert_eq!(0x7FFC, cpu.program_counter());
-                }
-            }
-        }
-
-        {
-            {
-                let mut memory = memory!(
-                    0x8000 => op_code,
-                    0x8001 => 0x06
-                );
-
-                let mut nes = super::NES::new();
-                nes.cpu.clear_flags(flag);
-                nes.execute_instruction(&mut memory);
-                let cpu = &nes.cpu;
-                if negative {
-                    assert_eq!(0x8008, cpu.program_counter());
-                } else {
-                    assert_eq!(0x8002, cpu.program_counter());
-                }
-            }
-
-            {
-                let mut memory = memory!(
-                    0x8000 => op_code,
-                    0x8001 => 0b1111_1010 // -6
-                );
-
-                let mut nes = super::NES::new();
-                nes.cpu.clear_flags(flag);
-                nes.execute_instruction(&mut memory);
-                let cpu = &nes.cpu;
-                if negative {
-                    assert_eq!(0x7FFC, cpu.program_counter());
-                } else {
-                    assert_eq!(0x8002, cpu.program_counter());
-                }
-            }
-        }
     }
 }
--- nes/src/opcodes.rs
+++ nes/src/opcodes.rs
@@ -317,3 +317,309 @@ pub const STX_ABSOLUTE: OpCode = 0x8E;
 pub const STY_ZERO_PAGE: OpCode = 0x84;
 pub const STY_ZERO_PAGE_X: OpCode = 0x94;
 pub const STY_ABSOLUTE: OpCode = 0x8C;
+
+#[cfg(test)]
+mod tests {
+    use cpu;
+    use memory::Memory;
+    use opcodes;
+
+    fn test_program(memory: &mut Memory, expected_cpu_states: &[cpu::CPU]) {
+        let mut cpu = cpu::CPU::new();
+
+        for &expected_cpu in expected_cpu_states.iter() {
+            super::execute_instruction(&mut cpu, memory);
+            assert_eq!(expected_cpu, cpu);
+        }
+    }
+
+    #[test]
+    fn instruction_test1() {
+        test_program(
+            &mut memory!(
+                0x00A5 => 0xF0,
+                //ADC $05
+                0x8000 => 0x69,
+                0x8001 => 0x05,
+
+                //AND $00
+                0x8002 => 0x29,
+                0x8003 => 0x00,
+                //ORA $05
+                0x8004 => opcodes::ORA_IMMEDIATE,
+                0x8005 => 0x05,
+
+                0x8006 => opcodes::ASL_ACCUMULATOR,
+
+                0x8007 => opcodes::SEC,
+                //SBC $05
+                0x8008 => opcodes::SBC_IMMEDIATE,
+                0x8009 => 0x05,
+
+                0x800A => opcodes::TAX,
+                0x800B => opcodes::TAY,
+                //STX Y
+                0x800C => opcodes::STX_ZERO_PAGE_Y,
+                0x800D => 0x0A,
+                0x800E => opcodes::AND_IMMEDIATE,
+                0x800F => 0x00,
+                0x8010 => opcodes::TAX,
+                //LDX Y
+                0x8011 => opcodes::LDX_ZERO_PAGE_Y,
+                0x8012 => 0x0A
+            ),
+            &[
+                cpu::CpuBuilder::new()
+                    .program_counter(0x8002)
+                    .accumulator(0x05)
+                    .build(),
+                cpu::CpuBuilder::new()
+                    .program_counter(0x8004)
+                    .flags(cpu::ZERO_FLAG)
+                    .accumulator(0x00)
+                    .build(),
+                cpu::CpuBuilder::new()
+                    .program_counter(0x8006)
+                    .accumulator(0x05)
+                    .build(),
+                cpu::CpuBuilder::new()
+                    .program_counter(0x8007)
+                    .accumulator(0x0A) //1010
+                    .build(),
+                cpu::CpuBuilder::new()
+                    .program_counter(0x8008)
+                    .flags(cpu::CARRY_FLAG)
+                    .accumulator(0x0A) //1010
+                    .build(),
+                cpu::CpuBuilder::new()
+                    .program_counter(0x800A)
+                    .flags(cpu::CARRY_FLAG)
+                    .accumulator(0x05)
+                    .build(),
+                cpu::CpuBuilder::new()
+                    .program_counter(0x800B)
+                    .flags(cpu::CARRY_FLAG)
+                    .accumulator(0x05)
+                    .register_x(0x05)
+                    .build(),
+                cpu::CpuBuilder::new()
+                    .program_counter(0x800C)
+                    .flags(cpu::CARRY_FLAG)
+                    .accumulator(0x05)
+                    .register_x(0x05)
+                    .register_y(0x05)
+                    .build(),
+                cpu::CpuBuilder::new()
+                    .program_counter(0x800E)
+                    .flags(cpu::CARRY_FLAG)
+                    .accumulator(0x05)
+                    .register_x(0x05)
+                    .register_y(0x05)
+                    .build(),
+                cpu::CpuBuilder::new()
+                    .program_counter(0x8010)
+                    .flags(cpu::CARRY_FLAG | cpu::ZERO_FLAG)
+                    .register_x(0x05)
+                    .register_y(0x05)
+                    .build(),
+                cpu::CpuBuilder::new()
+                    .program_counter(0x8011)
+                    .flags(cpu::CARRY_FLAG | cpu::ZERO_FLAG)
+                    .register_y(0x05)
+                    .build(),
+                cpu::CpuBuilder::new()
+                    .program_counter(0x8013)
+                    .flags(cpu::CARRY_FLAG)
+                    .register_x(0x05)
+                    .register_y(0x05)
+                    .build()
+            ]
+        )
+    }
+
+    #[test]
+    fn instruction_test2() {
+        test_program(
+            &mut memory!(
+                //ADC $05
+                0x8000 => 0x69,
+                0x8001 => 0x05,
+
+                0x8002 => opcodes::PHA,
+                0x8003 => opcodes::PLP
+            ),
+            &[
+                cpu::CpuBuilder::new()
+                    .program_counter(0x8002)
+                    .accumulator(0x05)
+                    .build(),
+                cpu::CpuBuilder::new()
+                    .program_counter(0x8003)
+                    .stack_pointer(0xFF)
+                    .accumulator(0x05)
+                    .build(),
+                cpu::CpuBuilder::new()
+                    .program_counter(0x8004)
+                    .flags(0x05)
+                    .accumulator(0x05)
+                    .build(),
+            ]
+        );
+    }
+
+    #[test]
+    fn test_subroutine() {
+        test_program(
+            &mut memory!(
+                0x8000 => opcodes::JSR_ABSOLUTE,
+                0x8001 => 0x20,
+                0x8002 => 0x80,
+
+                0x8003 => opcodes::ADC_IMMEDIATE,
+                0x8004 => 0x05,
+
+                //Sub routine
+                0x8020 => opcodes::LDA_IMMEDIATE,
+                0x8021 => 0x01,
+                0x8022 => opcodes::RTS
+            ),
+            &[
+                cpu::CpuBuilder::new()
+                    .program_counter(0x8020)
+                    .stack_pointer(0xFE)
+                    .build(),
+                cpu::CpuBuilder::new()
+                    .program_counter(0x8022)
+                    .stack_pointer(0xFE)
+                    .accumulator(0x01)
+                    .build(),
+                cpu::CpuBuilder::new()
+                    .program_counter(0x8003)
+                    .accumulator(0x01)
+                    .build(),
+                cpu::CpuBuilder::new()
+                    .program_counter(0x8005)
+                    .accumulator(0x06)
+                    .build(),
+            ]
+        )
+    }
+
+    #[test]
+    fn test_add_with_carry_immediate() {
+        test_instruction(
+            &mut memory!(
+                0x8000 => 0x69,
+                0x8001 => 0x05
+            ),
+            cpu::CpuBuilder::new()
+                .program_counter(0x8002)
+                .accumulator(0x05)
+                .build()
+        );
+    }
+
+    #[test]
+    fn test_add_with_carry_zero_page() {
+        test_instruction(
+            &mut memory!(
+                0x8000 => 0x65,
+                0x8001 => 0xAC,
+                0x00AC => 0x0A
+            ),
+            cpu::CpuBuilder::new()
+                .program_counter(0x8002)
+                .accumulator(10)
+                .build()
+        )
+    }
+
+    fn test_instruction(memory: &mut Memory, expected_cpu: cpu::CPU) {
+        let mut cpu = cpu::CPU::new();
+        super::execute_instruction(&mut cpu, memory);
+
+        assert_eq!(expected_cpu, cpu);
+    }
+
+    #[test]
+    fn test_branch_equal() {
+        test_branch(cpu::ZERO_FLAG, opcodes::BRANCH_EQUAL, false);
+        test_branch(cpu::ZERO_FLAG, opcodes::BRANCH_NOT_EQUAL, true);
+        test_branch(cpu::NEGATIVE_FLAG, opcodes::BRANCH_MINUS, false);
+        test_branch(cpu::NEGATIVE_FLAG, opcodes::BRANCH_PLUS, true);
+        test_branch(cpu::CARRY_FLAG, opcodes::BRANCH_CARRY_SET, false);
+        test_branch(cpu::CARRY_FLAG, opcodes::BRANCH_CARRY_CLEAR, true);
+        test_branch(cpu::OVERFLOW_FLAG, opcodes::BRANCH_OVERFLOW_SET, false);
+        test_branch(cpu::OVERFLOW_FLAG, opcodes::BRANCH_OVERFLOW_CLEAR, true);
+    }
+
+    fn test_branch(flag: u8, op_code: u8, negative: bool) {
+        {
+            {
+                let mut memory = memory!(
+                    0x8000 => op_code,
+                    0x8001 => 0x06
+                );
+
+                let mut cpu = cpu::CPU::new();
+                cpu.set_flags(flag);
+                super::execute_instruction(&mut cpu, &mut memory);
+                if negative {
+                    assert_eq!(0x8002, cpu.program_counter());
+                } else {
+                    assert_eq!(0x8008, cpu.program_counter());
+                }
+            }
+
+            {
+                let mut memory = memory!(
+                    0x8000 => op_code,
+                    0x8001 => 0b1111_1010 // -6
+                );
+
+                let mut cpu = cpu::CPU::new();
+                cpu.set_flags(flag);
+                super::execute_instruction(&mut cpu, &mut memory);
+                if negative {
+                    assert_eq!(0x8002, cpu.program_counter());
+                } else {
+                    assert_eq!(0x7FFC, cpu.program_counter());
+                }
+            }
+        }
+
+        {
+            {
+                let mut memory = memory!(
+                    0x8000 => op_code,
+                    0x8001 => 0x06
+                );
+
+                let mut cpu = cpu::CPU::new();
+                cpu.clear_flags(flag);
+                super::execute_instruction(&mut cpu, &mut memory);
+                if negative {
+                    assert_eq!(0x8008, cpu.program_counter());
+                } else {
+                    assert_eq!(0x8002, cpu.program_counter());
+                }
+            }
+
+            {
+                let mut memory = memory!(
+                    0x8000 => op_code,
+                    0x8001 => 0b1111_1010 // -6
+                );
+
+                let mut cpu = cpu::CPU::new();
+                cpu.clear_flags(flag);
+                super::execute_instruction(&mut cpu, &mut memory);
+                if negative {
+                    assert_eq!(0x7FFC, cpu.program_counter());
+                } else {
+                    assert_eq!(0x8002, cpu.program_counter());
+                }
+            }
+        }
+    }
+}
