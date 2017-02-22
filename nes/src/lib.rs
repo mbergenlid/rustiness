41ca3ccdc6ab46441aae12955f657530c69dd2a2
Marcus Bergenlid <marcus.bergenlid@gmail.com>
2017-02-22 13:58:31 +0100

    Added a clock NES.

--- nes/src/lib.rs
+++ nes/src/lib.rs
@@ -12,6 +12,9 @@ use memory::{BasicMemory, CPUMemory, Memory};
 use ppu::PPU;
 use ppu::screen::Screen;
 
+
+const NANOS_PER_CLOCK_CYCLE: u32 = 559;
+
 pub struct NES<T>
     where T: Screen + Sized
 {
@@ -19,7 +22,9 @@ pub struct NES<T>
     pub cycle_count: u64,
     pub ppu: PPU,
     pub op_codes: opcodes::OpCodes,
-    pub screen: Box<T>
+    pub screen: Box<T>,
+
+    clock: Clock,
 }
 
 impl <T> NES<T> where T: Screen + Sized {
@@ -35,6 +40,7 @@ impl <T> NES<T> where T: Screen + Sized {
             ppu: ppu,
             op_codes: opcodes::OpCodes::new(),
             screen: screen,
+            clock: Clock::start(),
         }
     }
 
@@ -42,6 +48,7 @@ impl <T> NES<T> where T: Screen + Sized {
         let cycles = self.op_codes.execute_instruction(&mut self.cpu, &mut CPUMemory::new(&mut self.ppu, memory));
         let nmi = self.ppu.update(cycles as u32, self.screen.as_mut());
         self.cycle_count += cycles as u64;
+        self.clock.tick(cycles as u32);
 
         if nmi {
             let cycles =
@@ -53,9 +60,79 @@ impl <T> NES<T> where T: Screen + Sized {
 
 }
 
+use std::thread::sleep;
+use std::time::{Instant, Duration};
+
+struct Clock {
+    start: Instant,
+    should_have_elapsed: Duration,
+}
+
+impl Clock {
+    fn start() -> Clock {
+        Clock {
+            start: Instant::now(),
+            should_have_elapsed: Duration::new(0,0),
+        }
+    }
+
+    fn tick(&mut self, cycles: u32) {
+        self.should_have_elapsed = self.should_have_elapsed + Duration::new(0, cycles*NANOS_PER_CLOCK_CYCLE);
+        let elapsed = self.start.elapsed();
+        if self.should_have_elapsed > elapsed {
+            sleep(self.should_have_elapsed - elapsed);
+        }
+    }
+
+}
+
 #[cfg(test)]
 mod test {
 
+    use std::time::{Instant, Duration};
+    use super::{Clock, NANOS_PER_CLOCK_CYCLE};
+    #[test]
+    fn clock_test() {
+        let start = Instant::now();
+        let mut clock = Clock::start();
+        clock.tick(10000);
+
+        let elapsed = start.elapsed();
+        let expected_duration = Duration::new(0, 10000*NANOS_PER_CLOCK_CYCLE);
+        assert!(elapsed >= expected_duration, "Should take at least {:?} but took {:?}", expected_duration, elapsed);
+        let expected_max_duration = Duration::new(0, 100000*NANOS_PER_CLOCK_CYCLE);
+        assert!(elapsed <= expected_max_duration, "Should take at most {:?} but took {:?}", expected_max_duration, elapsed);
+    }
+
+    #[test]
+    fn clock_test_2() {
+        let start = Instant::now();
+        let mut clock = Clock::start();
+        for _ in 0..5000 {
+            clock.tick(2);
+        }
+
+        let elapsed = start.elapsed();
+        let expected_duration = Duration::new(0, 10000*NANOS_PER_CLOCK_CYCLE);
+        assert!(elapsed >= expected_duration, "Should take at least {:?} but took {:?}", expected_duration, elapsed);
+        let expected_max_duration = Duration::new(0, 10100*NANOS_PER_CLOCK_CYCLE);
+        assert!(elapsed <= expected_max_duration, "Should take at most {:?} but took {:?}", expected_max_duration, elapsed);
+    }
+
+    #[test]
+    fn clock_should_handle_large_tick() {
+        let start = Instant::now();
+        let mut clock = Clock::start();
+        for _ in 0..5_000_000 {
+            clock.tick(2);
+        }
+
+        let elapsed = start.elapsed();
+        let expected_duration = Duration::new(5, 590_000_000);
+        assert!(elapsed >= expected_duration, "Should take at least {:?} but took {:?}", expected_duration, elapsed);
+        let expected_max_duration = Duration::new(5, 595_900_000);
+        assert!(elapsed <= expected_max_duration, "Should take at most {:?} but took {:?}", expected_max_duration, elapsed);
+    }
 //    use std::time::Instant;
 //    use memory::Memory;
 //    use ppu::PPU;
