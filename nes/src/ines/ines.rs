642f4034f4dfbcacfa54d9037e16674c01755181
Marcus Bergenlid <marcus.bergenlid@gmail.com>
2017-07-13 09:45:44 +0200

    Started implementing MMC1 mapper.

---
+++ nes/src/ines/ines.rs
@@ -0,0 +1,100 @@
+use std::fs::File;
+use std::io::Read;
+use memory::Memory;
+use ppu::ppumemory::{PPUMemory, Mirroring};
+
+pub type ROM = [u8];
+
+pub struct INes {
+    buffer: Vec<u8>,
+    pub num_prg_roms: u8,
+    pub num_chr_roms: u8,
+    pub mirroring: Mirroring,
+}
+
+impl <'a> INes  {
+    pub fn from_file(mut file: File) -> INes {
+        let mut buffer: Vec<u8> = vec!();
+        file.read_to_end(&mut buffer).unwrap();
+        let num_prg_roms = buffer[4];
+        let num_chr_roms = buffer[5];
+        let mirroring = if buffer[6] & 0x01 == 0 { Mirroring::Horizontal } else { Mirroring::Vertical };
+        INes {
+            buffer: buffer,
+            num_prg_roms: num_prg_roms,
+            num_chr_roms: num_chr_roms,
+            mirroring: mirroring,
+        }
+    }
+
+    pub fn read(file: &mut Read) -> INes {
+        let mut buffer: Vec<u8> = vec!();
+        file.read_to_end(&mut buffer).unwrap();
+        let num_prg_roms = buffer[4];
+        let num_chr_roms = buffer[5];
+        let mirroring = if buffer[6] & 0x01 == 0 { Mirroring::Horizontal } else { Mirroring::Vertical };
+        INes {
+            buffer: buffer,
+            num_prg_roms: num_prg_roms,
+            num_chr_roms: num_chr_roms,
+            mirroring: mirroring,
+        }
+    }
+
+    pub fn prg_rom(&self, index: usize) -> &ROM {
+        let rom_base: usize = 0x10 + index*0x4000;
+        &self.buffer[rom_base..(rom_base+0x4000)]
+    }
+
+    pub fn chr_rom(&self, index: usize) -> &ROM {
+        let chr_base = 0x10 + (self.num_prg_roms as usize)*0x4000;
+        let rom_base: usize = chr_base + index*0x2000;
+        &self.buffer[rom_base..(rom_base+0x2000)]
+    }
+
+    pub fn load(&self, cpu_memory: &mut Memory) {
+        cpu_memory.set_slice(0x8000, self.prg_rom(0));
+        if self.num_prg_roms == 1 {
+            cpu_memory.set_slice(0xC000, self.prg_rom(0));
+        } else if self.num_prg_roms == 2 {
+            cpu_memory.set_slice(0xC000, self.prg_rom(1));
+        } else {
+            panic!(".nes file contains more than 2 prg rom banks which is not allowed");
+        }
+    }
+
+    pub fn ppu_memory(&self) -> Box<PPUMemory> {
+        let mut ppu_mem = box PPUMemory::new(self.mirroring);
+        ppu_mem.set_slice(0x0000, self.chr_rom(0));
+        return ppu_mem;
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+
+    use std::fs::File;
+    use memory;
+    use memory::Memory;
+
+    #[test]
+    fn test() {
+        let file = File::open("src/ines/donkey_kong.nes").unwrap();
+        let ines = super::INes::from_file(file);
+
+        assert_eq!(1, ines.num_prg_roms);
+        assert_eq!(1, ines.num_chr_roms);
+
+        assert_eq!(ines.buffer[0x10..0x4010], *(ines.prg_rom(0)));
+
+        let mut memory = memory::BasicMemory::new();
+        ines.load(&mut memory);
+        assert_eq!(ines.buffer[0x10], memory.get(0x8000));
+
+        //should mirror 0xC0000 - 0xFFFF onto 0x8000-0xBFFF
+        for i in 0x8000..0xC000 {
+            assert_eq!(memory.get(i), memory.get(i + 0x4000));
+        }
+    }
+}
---
+++ nes/src/ines/mmc1.rs
@@ -0,0 +1,96 @@
+use memory::{Address, Memory, BasicMemory};
+use ines::INes;
+
+
+pub struct MMC1MemoryMapper {
+    internal_memory: BasicMemory,
+    prg_rom_banks: Vec<[u8; 0x4000]>,
+    low_prg_rom_bank: usize,
+    high_prg_rom_bank: usize,
+}
+
+impl MMC1MemoryMapper {
+    pub fn new(ines: &INes) -> MMC1MemoryMapper {
+        let num_prg_banks = ines.num_prg_roms as usize;
+        let mut prg_rom: Vec<[u8; 0x4000]> = Vec::with_capacity(num_prg_banks);
+        for rom_bank in 0..num_prg_banks {
+            let mut bank = [0; 0x4000];
+            bank.clone_from_slice(ines.prg_rom(rom_bank));
+            prg_rom.push(bank);
+        }
+        MMC1MemoryMapper {
+            internal_memory: BasicMemory::new(),
+            prg_rom_banks: prg_rom,
+            low_prg_rom_bank: 0,
+            high_prg_rom_bank: 15,
+        }
+    }
+}
+
+impl Memory for MMC1MemoryMapper {
+    fn get(&self, address: Address) -> u8 {
+        if address >= 0xC000 {
+            self.prg_rom_banks[self.high_prg_rom_bank][address as usize - 0xC000]
+        } else if address >= 0x8000 {
+            self.prg_rom_banks[self.low_prg_rom_bank][address as usize - 0x8000]
+        } else {
+            self.internal_memory.get(address)
+        }
+    }
+    fn set(&mut self, _: Address, _: u8) {
+        unimplemented!();
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::MMC1MemoryMapper;
+    use ines::INes;
+    use memory::Memory;
+
+    const INES_HEADER: [u8; 16] = [0x4e, 0x45, 0x53, 0x1a, 0x10, 0x00, 0x11, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
+
+    #[test]
+    fn initial_state() {
+        let rom = fill_banks();
+        let mapper = MMC1MemoryMapper::new(&INes::read(&mut ArrayStream(rom.iter())));
+        assert_eq!(1, mapper.get(0x8000)); //8000 -> switched to first bank.
+        assert_eq!(16, mapper.get(0xC000)); //C000 -> fixed to last bank
+        assert_eq!(0, mapper.get(0x1000)); //1000 -> mapped to NES internal memory
+    }
+
+    fn fill_banks() -> Vec<u8> {
+        let mut vec: Vec<u8> = Vec::with_capacity(16*0x4001);
+        for &d in INES_HEADER.iter() {
+            vec.push(d);
+        }
+        for x in 1..17 {
+            for _ in 0..0x4000 {
+                vec.push(x);
+            }
+        }
+        return vec;
+    }
+
+    struct ArrayStream<'a>(Iter<'a, u8>);
+
+    use std::slice::Iter;
+    use std::io::Read;
+    use std::io::Result;
+    impl <'a> Read for ArrayStream<'a> {
+        fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
+            let mut index = 0;
+            let mut next = self.0.next();
+            while next.is_some() {
+                buf[index] = *next.unwrap();
+                index += 1;
+                if index >= buf.len() {
+                    return Ok(index);
+                } 
+                next = self.0.next();
+            }
+            Ok(index)
+        }
+    }
+}
